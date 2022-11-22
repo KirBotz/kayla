661faa217a645fe369b57ac2ce511e12db904e1a
ZAKIFAISAL24 <90949422+ZAKIFAISAL24@users.noreply.github.com>
2022-11-22 10:41:40 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,9 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 global.hituet = 0
-global.gopayno = "0877-0504-8235"
-global.danano = "0877-0504-8233"
-global.shopeepayno = "0857-9814-5596"
-global.creator = "6287705048235@s.whatsapp.net"
+global.gopayno = "089531549103"
+global.danano = "089531549103"
+global.creator = "6285158738155@s.whatsapp.net"
 global.thumb = fs.readFileSync(`./image/thumb.png`)
 global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
 global.fake = `📍𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑲𝒂𝒚𝒍𝒂 𝑩𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑`
@@ -15,9 +14,9 @@ global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
-global.ownerNomor = '6287705048235'
-global.ownerName = 'Creator Kayla'
-global.ownerNumber = ["6287705048235@s.whatsapp.net"]
+global.ownerNomor = '089531549103'
+global.ownerName = 'Creator zaki'
+global.ownerNumber = ["6289531549103@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
     wait: 'Wait Sis Please be patient',
